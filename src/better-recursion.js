bb7db646a19181a44c30222e960111447a9bb315
Scott Richmond <s.richmond@utoronto.ca>
2019-03-26 21:29:01 -0400

    Add (possibly working) better recursion.

---
+++ src/better-recursion.js
@@ -0,0 +1,55 @@
+const init = (recursion_limit = 1000) => {  
+
+  Function.prototype.recursion_limit = recursion_limit
+
+  Function.prototype.recur = function(...args) {
+    this.recursion_count = this.recursion_count || 0
+
+    if (this.recursion_count > this.recursion_limit) {
+      console.warn(`Too much recursion in ${this.name || 'anonymous function'}.`)
+      this.recursion_count = undefined
+      return void null
+    }
+
+    this.recursion_count++
+    const result = this.apply(this, args)
+    this.recursion_count = undefined
+    return result
+
+  }
+  
+  const recursion_symbol = Symbol("Recursion symbol.")
+
+  const is_recursive_call = result => 
+    result != null && 
+    typeof result === 'object' &&
+    recursion_symbol in result
+  
+  Function.tco = f => {
+    f.recur = (...args) => ({recur: recursion_symbol, args})
+
+    const call = (...args) => {
+      let result = f(...args)
+      
+      let recursion_count = 0
+
+      while (is_recursive_call(result)) {
+        if (recursion_count > f.recursion_limit) {
+          console.warn(`Too much recursion in ${f.name || 'anonymous function'}.`)
+          return void null
+        }
+        recursion_count++
+        result = f(...result.args)
+      }
+
+      return result
+    }
+
+    return Object.defineProperty(call, 'name', {
+      value: f.name,
+      enumerable: false
+    })
+  }
+}
+
+module.exports = {init}
